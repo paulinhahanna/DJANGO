82c9128401dbd98a3f015059b9deb57930e8df35
ana paula^C <paulinhaanna04gmail.com>
2024-01-11 14:15:38 -0300

    site atualizado

---
+++ setimo_projeto/home/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpRequest
+from .models import Produto
+
+
+def home(request):
+    meus_produtos = Produto.objects.all()
+    context ={
+        'produtos':meus_produtos,
+    }
+    return render (request,'home/index.html',context,)
+
+
